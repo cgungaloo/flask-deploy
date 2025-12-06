77ee03157ce28956cb15aeb38c9d647cbaf744e6
chris gungaloo <chrisgungaloo@chriss-MacBook-Pro.local>
2025-12-06 19:46:02 +0000

    adding db url check

--- db/database.py
+++ db/database.py
@@ -4,6 +4,10 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+asyncpg://", 1)
+
+
 engine = create_async_engine(DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(
